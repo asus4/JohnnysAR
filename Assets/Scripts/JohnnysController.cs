acda7eab11623a439130c0104ad7af37c7d9abb7
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-10-11 14:47:16 +0200

    First sample 

---
+++ Assets/Scripts/JohnnysController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+
+namespace Johnnys
+{
+
+    public class JohnnysController : MonoBehaviour
+    {
+        [SerializeField]
+        ARHumanBodyManager humanBodyManager;
+
+        [SerializeField]
+        RawImage image;
+
+
+        void Start()
+        {
+            Debug.Log("Started");
+        }
+
+        void Update()
+        {
+            var subsystem = humanBodyManager.subsystem;
+            if (subsystem == null)
+            {
+                return;
+            }
+
+            var stencil = humanBodyManager.humanStencilTexture;
+            image.texture = stencil;
+        }
+    }
+}
